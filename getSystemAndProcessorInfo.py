a2fd8425b6b1e4f9e954f9997e5b3130cf604d26
mohitesachin217 <7931096+mohitesachin217@users.noreply.github.com>
2023-08-24 21:34:30 +0530

    Create getSystemAndProcessorInfo.py
    Added file for getting os and processor info

---
+++ getSystemAndProcessorInfo.py
@@ -0,0 +1,15 @@
+import platform
+
+def get_processor_info():
+    system_info = platform.uname()
+    
+    print("Processor Information:")
+    print(f"  System: {system_info.system}")
+    print(f"  Node Name: {system_info.node}")
+    print(f"  Release: {system_info.release}")
+    print(f"  Version: {system_info.version}")
+    print(f"  Machine: {system_info.machine}")
+    print(f"  Processor: {system_info.processor}")
+    
+if __name__ == "__main__":
+    get_processor_info()
